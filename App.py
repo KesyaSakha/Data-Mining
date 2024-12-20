e2e6b2a6df30b0ee524e53876183f73a38646532
Dimas Alfayza R.E <dimas.alfaiza7@gmail.com>
2024-12-20 09:57:09 +0700

    Update App.py

--- App.py
+++ App.py
@@ -118,7 +118,7 @@ with st.expander("Skenario Eksperimen"):
     # Evaluasi
     print("Skenario 1: TF-IDF + Naive Bayes")
     print(classification_report(y_test, y_pred_nb_tfidf, target_names=le.classes_))
-    st.write("Selesai.")
+    st.write("Berhasil.")
     
     st.subheader("Skenario 2: Menggunakan TF-IDF dan SVM")
     # SVM
